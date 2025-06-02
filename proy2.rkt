f5a0d973ad67a5923936afa59eddc022b18c6f4a
anabms0101 <166447167+anabms0101@users.noreply.github.com>
2025-06-01 18:16:36 -0600

    Update proy2.rkt

--- proy2.rkt
+++ proy2.rkt
@@ -1,1 +1,29 @@
+#lang racket
+(require racket/trace)
 
+(define (es-variable? x)
+  (let* ([str (if (symbol? x) (symbol->string x) x)]
+         [lst (string->list str)])
+    (cond
+      [(empty? lst) #f]
+      [else (char=? (first lst) #\?)])))
+
+(define (es-atomo? x)
+  (let* ([str (if (symbol? x) (symbol->string x) x)]
+         [lst (string->list str)])
+    (cond
+      [(empty? lst) #f]
+      [(char=? (first lst) #\?) #f]
+      [else #t])))
+
+(define (es-variable-interna? x)
+  (let* ([str (if (symbol? x) (symbol->string x) x)]
+         [lst (string->list str)])
+    (and (char=? (first lst) #\?)
+         (char=? (second lst) #\_))))
+
+(define (asociar variable valor asociaciones)
+  (if (es-variable? variable)
+      (cons (cons variable valor) asociaciones)
+      asociaciones)) 
+(trace asociar)
